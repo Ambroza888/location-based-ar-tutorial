d068e862225818eb53ac4349a84e236998035e29
Veselin <Ambroza8888@gmail.com>
2021-04-26 21:55:32 -0700

    wqewqe

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
-document.getElementById('location').innerHTML = distanceMsg;
+document.getElementById('location').value = distanceMsg;
