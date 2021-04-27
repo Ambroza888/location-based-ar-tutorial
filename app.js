9577c2c2f4612234341858c0ad642f7b7dd36058
Veselin <Ambroza8888@gmail.com>
2021-04-27 14:15:47 -0700

    wqeqw

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
 // document.getElementById('location').value = distanceMsg;
-const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+const distanceMsg = document.querySelector('#imgID').getAttribute('distanceMsg');
 console.log(distanceMsg);   // "890 meters"
 console.log('yooo');
